86ce2fb468d5d5851c3c6a4841835e0b09ceb996
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-24 12:19:22 -0500

    commit tree feed1f4f57b03a9cdc287c563cba06b6891eabc6

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,43 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  changeListener = (e) => {
+    this.setState({[e.target.name]: e.target.value}, () => console.log(this.state))
+  }
+
+  submitListener = (e) => {
+    e.preventDefault()
+    if (this.state.username.length === 0 || this.state.password.length === 0) {
+      alert("Enter Login Details")
+    } else {
+      this.props.handleLogin(this.state)
+      this.setState({ username: "", password: "" })
+    }
+    
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitListener}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.changeListener}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.changeListener}/>
           </label>
         </div>
         <div>
